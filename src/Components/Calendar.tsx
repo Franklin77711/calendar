3c9bb7810b7bcf5d076b038f691a123002994fbc
Franklin77711 <robi1000350@gmail.com>
2023-03-02 12:30:38 +0100

    made days dynamic, and set active class on click, next is to handle inactive days

--- src/Components/Calendar.tsx
+++ src/Components/Calendar.tsx
@@ -1,16 +1,74 @@
+import { useState, useEffect } from "react";
+import * as React from "react";
+
+
+
 function Calendar(){
+
+const allmonth: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+const [currentMonthByNum, setcurrentMonthByNum] = React.useState<number>(()=>{
+    const date = new Date;
+    return date.getMonth() ;
+});
+const [currentYear, setCurrentYear] = React.useState<number>(()=>{
+    const date = new Date;
+    const curryear = date.getFullYear();
+    return curryear
+});
+const [currentMonthDays, setcurrentMonthDays] = React.useState<number>(()=>{
+    return new Date(currentYear, currentMonthByNum +1, 0).getDate();
+})
+const [daysInMonth, setDaysInMonth] = React.useState<number[]>([]);
+const [isActive, setIsActive] = React.useState<number>();
+
+const changeMonthNeg = ()=>{ 
+    if( currentMonthByNum <= 0){
+        setcurrentMonthByNum(11)
+        setCurrentYear(currentYear-1)
+    }else{
+        setcurrentMonthByNum(currentMonthByNum - 1)
+    }
+    
+}
+const changeMonthPos = ()=>{ 
+    if (currentMonthByNum >= 11){
+        setcurrentMonthByNum(0)
+        setCurrentYear(currentYear+1)
+    }else{
+        setcurrentMonthByNum(currentMonthByNum + 1)
+ 
+    }
+}
+useEffect(()=>{
+    setcurrentMonthDays(new Date(currentYear, currentMonthByNum + 1, 0).getDate());
+}, [currentMonthByNum])
+
+useEffect(()=>{
+    console.log('render')
+    setDaysInMonth([])
+    for(let i = 1; i <= currentMonthDays; i++){
+        setDaysInMonth(current =>[...current, i])
+    }
+}, [currentMonthDays])
+
+const applyActiveClass = (index:number)=>{
+    setIsActive(index)
+}
+
 return(
         <div id="calendar-wrapper">
             <div id="calendar-nav">
-                <div>
-                    March 2023
+                <div id="current-date">
+                    {allmonth[currentMonthByNum]} {currentYear}
                 </div>
                 <div id="month-switcher">
-                    <div id="prev-month" className="switch-button"> {'<'} </div>
-                    <div id="next-month" className="switch-button"> {'>'} </div>
+                    <div id="prev-month" className="switch-button" onClick={changeMonthNeg}> {'<'} </div>
+                    <div id="next-month" className="switch-button" onClick={changeMonthPos}> {'>'} </div>
                 </div>
             </div>
             <div id="day-names">
+                <ul className="day-names-list-ul">
                     <li>Monday</li>
                     <li>Tuesday</li>
                     <li>Wednesday</li>
@@ -18,43 +76,18 @@ return(
                     <li>Friday</li>
                     <li>Saturday</li>
                     <li>Sunday</li>
+                </ul>
             </div>
             <div id="days-num">
-                <li className="inactive">30</li>
-                <li className="inactive">31</li>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-                <li>7</li>
-                <li>8</li>
-                <li>9</li>
-                <li>10</li>
-                <li>11</li>
-                <li>12</li>
-                <li>13</li>
-                <li>14</li>
-                <li>15</li>
-                <li>16</li>
-                <li>17</li>
-                <li>18</li>
-                <li>19</li>
-                <li>20</li>
-                <li>21</li>
-                <li>22</li>
-                <li>23</li>
-                <li>24</li>
-                <li>25</li>
-                <li>26</li>
-                <li>27</li>
-                <li className="active">28</li>
-                <li>29</li>
-                <li>30</li>
-                <li>31</li>
-                <li className="inactive">1</li>
-                <li className="inactive">2</li>
+            <ul className="day-list-ul">
+                    <li className="inactive">30</li>
+                    <li className="inactive">31</li>
+                    {daysInMonth.map((day, index)=>{
+                        return <li key={index} onClick={()=>{applyActiveClass(index)}} className={`${isActive === index ? 'active':''}`}>{day}</li>
+                    })}
+                    <li className="inactive">1</li>
+                    <li className="inactive">2</li>
+                </ul>
             </div>
         </div>
 )
